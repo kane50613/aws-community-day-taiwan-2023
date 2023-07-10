352a79de9790a36f1a428fd5df4329cae5f0503d
Gary50613 <50613gary@gmail.com>
2023-07-10 21:19:46 +0800

    add sections

--- src/components/WhyReason.tsx
+++ src/components/WhyReason.tsx
@@ -8,7 +8,7 @@ export const WhyReason: FC<
 > = ({ title, description, children }) => (
 	<div className="flex gap-4 items-center">
 		<div
-			className="rounded-lg w-20 md:w-24 h-20 md:h-24 grid place-items-center flex-shrink-0"
+			className="rounded-lg w-20 h-20 grid place-items-center flex-shrink-0"
 			style={{
 				background: "linear-gradient(135deg, #404040 0%, #000 100%)",
 			}}
@@ -17,7 +17,7 @@ export const WhyReason: FC<
 		</div>
 		<div className="flex flex-col md:gap-2">
 			<span className="text-lg md:text-xl font-medium">{title}</span>
-			<span className="text-md md:text-lg">{description}</span>
+			<span className="text-md">{description}</span>
 		</div>
 	</div>
 );
