3e0c8e208bd3aabeeb2d2fcd4fd4a8a47a04a7f6
Gary50613 <50613gary@gmail.com>
2023-07-10 17:22:51 +0800

    add email

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -9,7 +9,7 @@ export const CTAButton: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({
 			background: "linear-gradient(45deg, #FFA857 0%, #FFDB59 100%)",
 			boxShadow: "0px 4px 12px 4px rgba(255, 186, 83, 0.25)",
 		}}
-		className="gap-3 py-2 px-6 rounded-md text-sm md:text-md w-fit"
+		className="gap-3 py-2 px-6 rounded-lg text-sm md:text-md w-fit"
 		{...props}
 	>
 		訂閱電子報
---
+++ src/components/EmailForm.tsx
@@ -0,0 +1,49 @@
+import { CTAButton } from "@/components/CTAButton";
+import { FormEvent, useCallback, useRef } from "react";
+
+export const EmailForm = () => {
+	const email = useRef<HTMLInputElement>(null);
+
+	const onSubmit = useCallback(
+		(event: FormEvent) => {
+			event.preventDefault();
+
+			if (!email.current) return;
+
+			const { value } = email.current;
+
+			if (!/^\S+@\S+\.\S+$/.test(value)) return alert("錯誤的 Email");
+
+			fetch(
+				"https://pobtd17gf3.execute-api.us-east-1.amazonaws.com/Prod",
+				{
+					method: "POST",
+					body: JSON.stringify({
+						email: email.current.value,
+					}),
+				},
+			)
+				.then((response) => {
+					if (!response.ok)
+						alert(`訂閱電子報發生了錯誤: ${response.statusText}`);
+					else alert("成功訂閱電子報");
+				})
+				.catch((e) => {
+					alert(`送出請求時發生了錯誤: ${e}`);
+				});
+		},
+		[email.current],
+	);
+
+	return (
+		<form className="flex gap-2 w-full" onSubmit={onSubmit}>
+			<input
+				ref={email}
+				className="bg-gray-200 flex-grow rounded-lg px-4 outline-gray-700"
+				type="email"
+				required
+			/>
+			<CTAButton type="submit" />
+		</form>
+	);
+};
