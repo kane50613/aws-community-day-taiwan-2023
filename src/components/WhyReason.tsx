9c95220a0ce977ecde1ad6f4de793ac11e0166a1
Gary50613 <50613gary@gmail.com>
2023-07-10 16:14:02 +0800

    basic RWD

--- src/components/WhyReason.tsx
+++ src/components/WhyReason.tsx
@@ -8,16 +8,16 @@ export const WhyReason: FC<
 > = ({ title, description, children }) => (
 	<div className="flex gap-4 items-center">
 		<div
-			className="rounded-lg w-24 h-24 grid place-items-center flex-shrink-0"
+			className="rounded-lg w-20 md:w-24 h-20 md:h-24 grid place-items-center flex-shrink-0"
 			style={{
 				background: "linear-gradient(135deg, #404040 0%, #000 100%)",
 			}}
 		>
 			{children}
 		</div>
-		<div className="flex flex-col gap-2">
-			<span className="text-xl font-medium">{title}</span>
-			<span className="text-lg">{description}</span>
+		<div className="flex flex-col md:gap-2">
+			<span className="text-lg md:text-xl font-medium">{title}</span>
+			<span className="text-md md:text-lg">{description}</span>
 		</div>
 	</div>
 );
