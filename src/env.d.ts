112d8dd14f3ef8c630f1513e603f722011ace256
Gary50613 <50613gary@gmail.com>
2023-07-31 12:36:14 +0800

    staging brands

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="astro/client-image" />
+
+interface ImportMetaEnv {
+	readonly NODE_ENV?: "staging" | "production";
+	// more env variables...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
