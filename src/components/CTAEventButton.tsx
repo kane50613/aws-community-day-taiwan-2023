007648ae9bad9243072056d3ef7cb96074e13c6a
Gary50613 <50613gary@gmail.com>
2023-08-01 20:02:48 +0800

    fix text align

--- src/components/CTAEventButton.tsx
+++ src/components/CTAEventButton.tsx
@@ -1,4 +1,5 @@
 import type { ButtonHTMLAttributes, FC } from "react";
+import { cn } from "@/lib/utils";
 
 export const CTAEventButton: FC<ButtonHTMLAttributes<HTMLAnchorElement>> = ({
 	children,
@@ -10,8 +11,11 @@ export const CTAEventButton: FC<ButtonHTMLAttributes<HTMLAnchorElement>> = ({
 			background: "linear-gradient(to right, #4776E6 0%, #8E54E9 100%)",
 			boxShadow: "0px 4px 12px 2px rgba(142, 84, 233, 0.25)",
 		}}
-		className="md:text-md w-fit gap-3 whitespace-nowrap rounded-lg px-6 py-2 text-sm text-white"
 		{...props}
+		className={cn(
+			"md:text-md w-fit gap-3 whitespace-nowrap rounded-lg px-6 py-2 text-sm text-white",
+			props.className,
+		)}
 	>
 		立即報名
 	</a>
