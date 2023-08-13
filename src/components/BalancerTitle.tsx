ffea7d63188ce6b94be4975def9ca88e3c2c2b4f
Gary50613 <50613gary@gmail.com>
2023-08-13 22:25:53 +0800

    fix balancer import

--- src/components/BalancerTitle.tsx
+++ src/components/BalancerTitle.tsx
@@ -1,5 +1,5 @@
 import type { ComponentProps, FC } from "react";
-import Balancer from "react-wrap-balancer";
+import { Balancer } from "react-wrap-balancer";
 
 // a wrapper for client hydrated component or astro will break!
 export const BalancerTitle: FC<ComponentProps<typeof Balancer>> = (props) => (
