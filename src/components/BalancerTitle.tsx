205e280d444be2da556d094cd2da14d401803641
Gary50613 <50613gary@gmail.com>
2023-08-13 22:22:29 +0800

    add avatars

---
+++ src/components/BalancerTitle.tsx
@@ -0,0 +1,7 @@
+import type { ComponentProps, FC } from "react";
+import Balancer from "react-wrap-balancer";
+
+// a wrapper for client hydrated component or astro will break!
+export const BalancerTitle: FC<ComponentProps<typeof Balancer>> = (props) => (
+	<Balancer {...props} />
+);
